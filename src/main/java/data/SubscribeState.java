d669f05603c541d4558cb9b6e3a464413ce3884e
Dmitry <mr.dimenter@gmail.com>
2020-11-11 22:30:47 +0400

    Added new class "SubscribeState"

---
+++ src/main/java/data/SubscribeState.java
@@ -0,0 +1,29 @@
+package data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubscribeState {
+    private double total; //Общая стоимость подписок
+    private Map<String, Double> subscribeState;
+
+    public SubscribeState() {
+        this.subscribeState = new HashMap<>();
+
+    }
+
+    public void setSubscribeState(String nameService, double price) {
+        subscribeState.put(nameService, price);
+    }
+
+    public double getTotal() {
+        for (double sum : subscribeState.values()) total += sum;
+        return total;
+    }
+
+    public Map<String, Double> getSubscribeState() {
+        return subscribeState;
+    }
+
+}
+
